9394aa60cf6e785a9353f0488da3b67636176518
chenqiu <chenqiu@1943257728010728.onaliyun.com>
2022-08-02 16:49:13 +0800

    登录表单样式添加

--- react_tests/src/pages/Login/index.js
+++ react_tests/src/pages/Login/index.js
@@ -1,5 +1,5 @@
-import { Card , Button ,Form} from 'antd'
-// import './index.scss'
+import { Card , Button ,Form ,Input ,Checkbox } from 'antd'
+import './index.scss'
 
 const Login = () => {
   const onFinish = (values)=>{
@@ -8,15 +8,18 @@ const Login = () => {
   const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
         };
+  const submit =(val)=>{
+        console.log(val,'看看val有打印不？')
+  };
   return (
     <div className="login">
-      <Card className="login-container">
-        <Button type="primary">点击也登录不了</Button>
+      <Card className='login-card' title="登录" bordered={false}>
         {/* 登录表单 */}
         <Form
+        className='login-form'
         name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 20 }}
         initialValues={{ remember: true }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
@@ -25,26 +28,26 @@ const Login = () => {
       <Form.Item
         label="用户名"
         name="username"
-        rules={[{ required: true, message: '填用户名啊喂!' }]}
+        rules={[{ required: true, message: 'Please input your username!' }]}
       >
         <Input />
       </Form.Item>
 
       <Form.Item
-        label="Password"
+        label="密码"
         name="password"
         rules={[{ required: true, message: 'Please input your password!' }]}
       >
         <Input.Password />
       </Form.Item>
 
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-        <Checkbox>Remember me</Checkbox>
+      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 1, span: 8 }}>
+        <Checkbox>鸡猪我</Checkbox>
       </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Form.Item wrapperCol={{ offset: 4, span: 8 }}>
         <Button type="primary" htmlType="submit">
-          Submit
+          点击也登录不了
         </Button>
       </Form.Item>
     </Form>
